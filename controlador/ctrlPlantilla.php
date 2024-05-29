05753312a5b3f66a9b4ac87efbe71563cb0231ea
Zaywop06 <160089073+Zaywop06@users.noreply.github.com>
2024-05-28 18:07:27 -0600

    Commit

--- controlador/ctrlPlantilla.php
+++ controlador/ctrlPlantilla.php
@@ -8,7 +8,7 @@
 
         static public function ctrlRuta()
         {
-            return "https://8bb1-201-102-167-254.ngrok-free.app/vista/recursos";
+            return "https://2e5b-201-102-167-254.ngrok-free.app/vista/recursos";
         }
     }
 ?>
